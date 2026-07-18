4b0d0a42156bbe4a51c5849800bf56684427d98b
GuilhermeDev6 <gg6.dev@gmail.com>
2026-07-18 11:05:39 -0300

    refactor(Martona-java) rename for Maratona-java

--- src/estudo/devdojo/maratonajava/introducao/Aula01OlaMundo.java
+++ src/estudo/devdojo/maratonajava/introducao/Aula01OlaMundo.java
@@ -1,7 +1,8 @@
 package estudo.devdojo.maratonajava.introducao;
 
 public class Aula01OlaMundo {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Olá mundo");
     }
 }
+
--- src/estudo/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/estudo/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,18 +1,22 @@
 package estudo.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
-    static void main(String[] args) {
+     public static void main(String[] args) {
         //int, double, float, chat, byte, short, long, boolean
-        int age = 10;
+        int age = (int)100000000000L; //casting
         long number = 100000;
         double salaryDouble = 2600;
-        double salaryFloat = 3000;
+        float salaryFloat = 3000;
         byte ageByte = 10;
         short ageShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caracter = 'M';
+
+        String name = "Guilherme";
+        //var nome2 = "Vegeta"; essa funcionalidade foi adicionada no java 10, porém o level do projeto está no java 8
         System.out.println("A idade é: "+age+" anos");
         System.out.println(verdadeiro);
+        System.out.println("Oi meu nome é "+name);
     }
 }
