232d5abec7fbee57640de2cd84834fecb472e132
GuilhermeDev6 <gg6.dev@gmail.com>
2026-07-18 11:59:47 -0300

    feat: types of arithmetic operators

---
+++ src/estudo/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package estudo.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int number01 = 10;
+        int number02 = 20;
+        int result = number02 + number01;
+        //System.out.println(number02+number01);
+        System.out.println(result);
+    }
+}
