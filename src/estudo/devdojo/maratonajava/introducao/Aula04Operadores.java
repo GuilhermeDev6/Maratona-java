1bc823b08695f4a221f68e88ab0d1d1acf227121
GuilhermeDev6 <gg6.dev@gmail.com>
2026-07-21 21:53:08 -0300

    feat: types of logic operators

--- src/estudo/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/estudo/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -8,5 +8,38 @@ public class Aula04Operadores {
         int result = number02 + number01;
         //System.out.println(number02+number01);
         System.out.println(result);
+
+        //% RESTO
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        /*relational operators         < > <= >= == !=
+         */
+        boolean isDezMariorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMariorQueVinte " + isDezMariorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
+
+        //logical operators && (AND) || (or) !
+        int age = 29;
+        float salary = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = age >= 30 && salary >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = age < 30 && salary >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double accountSalaryBalance = 200;
+        double accountSavingsBalance = 10000;
+        float pricePlastation = 5000F;
+        boolean isPlaystationFivePurchasable = accountSalaryBalance > pricePlastation || accountSavingsBalance > pricePlastation;
+        System.out.println("isPlaystationFivePurchasable "+isPlaystationFivePurchasable);
+
     }
 }
