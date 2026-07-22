476bcf90f8c5a968b773046000ad527d15966184
GuilhermeDev6 <gg6.dev@gmail.com>
2026-07-21 22:11:33 -0300

    feat: types of assignment operators

--- src/estudo/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/estudo/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -39,7 +39,31 @@ public class Aula04Operadores {
         double accountSavingsBalance = 10000;
         float pricePlastation = 5000F;
         boolean isPlaystationFivePurchasable = accountSalaryBalance > pricePlastation || accountSavingsBalance > pricePlastation;
-        System.out.println("isPlaystationFivePurchasable "+isPlaystationFivePurchasable);
+        System.out.println("isPlaystationFivePurchasable " + isPlaystationFivePurchasable);
+
+        //Operadores de atribuição = += -= *= /= %=
+
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2; // 3600
+        bonus /= 2; // 1800
+        bonus %= 2; // 0
+        System.out.println(bonus);
+
+        //++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println(contador);
+        int contador2 = 0;
+        System.out.println(contador2++);// Primeiro executa oque tem que ser executado dentro da linha
+        System.out.println(contador2);// Depois incrimenta oque tem que ser incrementado
+
+
 
     }
 }
