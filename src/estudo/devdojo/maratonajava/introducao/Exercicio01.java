a29e2cda89fc2f947eed64e3a935d55e1e19130e
GuilhermeDev6 <gg6.dev@gmail.com>
2026-07-23 21:51:13 -0300

    feat: exercise about gross salary

---
+++ src/estudo/devdojo/maratonajava/introducao/Exercicio01.java
@@ -0,0 +1,21 @@
+package estudo.devdojo.maratonajava.introducao;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        double salary = 70000;
+
+        double tax1 = salary * (9.7 / 100);
+        double tax2 = salary * (37.35 / 100);
+        double tax3 = salary * (49.50 / 100);
+        double afterTax;
+
+        if (salary <= 34712) {
+            afterTax = tax1;
+        } else if (salary > 34712 && salary <= 68507) {
+            afterTax = tax2;
+        } else {
+            afterTax = tax3;
+        }
+        System.out.println("Seu salario após os impostos é: "+afterTax);
+    }
+}
