eac6f6f5764eb25a55008d265b68f56d55bfc4f3
GuilhermeDev6 <gg6.dev@gmail.com>
2026-07-25 17:59:10 -0300

    feat: iteration construct

---
+++ src/estudo/devdojo/maratonajava/introducao/Aula05EstruturasDeRepeticao01Exercicio03.java
@@ -0,0 +1,31 @@
+package estudo.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasDeRepeticao01Exercicio03 {
+    public static void main(String[] args) {
+       /* //Imprima todos os numeros pares de 0 até 1000
+        int par = 0;
+        while (par <= 1000) {
+            System.out.println(par += 2);
+        }*/
+
+        //Da maneira abaixo mesmo se o numero inicial alterar ainda irá retornar um numero par
+        int par = 0;
+        par = 0;
+        while (par <= 1000) {
+            if (par % 2 == 0) {
+                System.out.println(par += 2);
+            }
+        }
+
+        /*par = 0;
+        do {
+            System.out.println(par += 2);
+
+        } while (par <= 1000);*/
+
+        /*for (int i = 0; i <= 1000; i += 2) {
+            System.out.println("For " + i);
+
+        }*/
+    }
+}
