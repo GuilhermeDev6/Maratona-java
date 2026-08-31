3d2ef961c05eddccd61cbf87d4482d9f5dd37cb3
GuilhermeDev6 <gg6.dev@gmail.com>
2026-08-31 06:07:59 -0300

    feat: Introduction to Object Orientation - Cohesion

--- src/estudo/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
+++ src/estudo/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -4,4 +4,9 @@ public class Estudante {
     public String name = "Naruto";
     public int age;
     public char gender;
+
+    // CLasse deve ser focada em apenas uma responsabildiade
+    //Public String nameTeacher;
+    //Public int ageTeacher;
+    //Public String genderTeacher;
 }
