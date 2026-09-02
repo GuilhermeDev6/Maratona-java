3efaa36b1e884b170a0a7b6e20b54f8bf72d3fb2
GuilhermeDev6 <gg6.dev@gmail.com>
2026-09-01 21:56:42 -0300

    feat: Exercise on the Car class

--- src/estudo/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarTeste1.java
+++ src/estudo/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarTeste1.java
@@ -14,6 +14,9 @@ public class CarTeste1 {
         car2.model = "Caravan";
         car2.year = 1978;
 
+        //Variavel do tipo referente pode fazer referencia para outro variavel
+        //car = car2;
+
         System.out.println("Era uma vez dois amigos, apaixonados por carros, Ricardinho ACF com o seu conhecido " + car.name + " do modelo " + car.model + " ano " + car.year);
         System.out.println("E conhecido Alemão da Caravan, que era apaixonado na sua " + car2.name + " do modelo " + car2.model + " ano " + car2.year);
     }
